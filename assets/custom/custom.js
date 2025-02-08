685fbf87584aba5e5e6a7a9dc415044fc45f5301
woojin065 <123820750+woojin065@users.noreply.github.com>
2025-02-08 10:53:26 +0900

    feat: 대표이미지 내용에 안보이게 수정3

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -1,6 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
+const observer = new MutationObserver(() => {
   let elements = document.querySelectorAll('.popup.img-link.preview-img');
   elements.forEach((el) => {
     el.style.display = 'none';
   });
 });
+
+observer.observe(document.body, { childList: true, subtree: true });
